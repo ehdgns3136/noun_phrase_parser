35062f487b7d5e29d3474430b0ac9cf887945626
LAPTOP-13CFDG55\김동훈 <ehdgns3136@gmail.com>
2018-06-10 14:15:28 +0900

    implement noun parser with nltk library, but it cannot parse the noun which has adjective clause

--- untitled/PN_Parser.py
+++ untitled/PN_Parser.py
@@ -1,17 +1,45 @@
 import nltk
-nltk.download('averaged_perceptron_tagger')
+# nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 
-sentences = [
-    "hello, my name is donghoon kim",
-    "nice to meet you! Where are you from?",
-    "I'm from Korean. What about you?"
-]
+def parse_np():
+    grammar = """
+        EPN: {<DT|PRP.>?<JJ.*|CD>*<NN.*|CD|PRP>+} # element noun phrase
+        BPN: {<EPN>(<IN><EPN>)*} # bunch of element noun phrase
+        GPN: {<VBG><VBN>?(<BPN>|<TO|IN><BPN>)} # greund noun phrase
+        TPN: {<TO><VB>(<BPN>|<TO|IN><BPN>)} # to noun phrase
+        
+        NP: {<BPN>*}
+            {<GPN>} 
+            {<TPN>}
+            {<BPN><GPN|TPN>*}
+    """
+    the man
+    the pretty man
+    if word == DT:
+        if word2 == NN:
+            명사구다!
+        if word2 == JJ:
+            if word3 == NN:
+                명사구디!
 
-for sentence in sentences:
-    nltk.pos_tag_sents(sentence)
-    token = word_tokenize(sentence)
-    print(token)
-    tag = nltk.pos_tag(token)
-    print(tag)
+    with open('sentences.txt') as file:
+        sentences = file.readlines()
 
+    for sentence in sentences:
+        sentence = sentence.replace("\n", "")
+        token = word_tokenize(sentence)
+        tag = nltk.pos_tag(token)
+        cp = nltk.RegexpParser(grammar)
+        tree = cp.parse(tag)
+
+        terms = []
+        for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
+            terms.append(subtree.leaves())
+
+        print(sentence)
+        for term in terms:
+            print(term)
+        print()
+
+parse_np()
